886c1160b5a812a084fa50b0f6102b8261739cc2
edbertlinardi <elinardi7@hotmail.com>
2017-11-30 20:31:00 -0600

    added module exports

--- backend/aws-api/api-call.js
+++ backend/aws-api/api-call.js
@@ -1,15 +1,18 @@
 amazon = require('aws-product-api');
 
-var RESTobj = new amazon("UxE++3FH2C8wTyYyOwOKloP7bQZOumol329NNiHr", "AKIAIL6MEBROY6QG653Q", " cs498rkfp-20");
+let RESTobj = new amazon("UxE++3FH2C8wTyYyOwOKloP7bQZOumol329NNiHr", "AKIAIL6MEBROY6QG653Q", " cs498rkfp-20");
 
-RESTobj.Query({
-  Operation: "ItemLookup",
-  ItemId: 'B074SGVDK3',
-  ResponseGroup: "Small"
-}, function(err, results) {
-  if(err){
-    console.log(err);
-  }else{
-    console.log(results[0]['Item']);
-  }
-});
+module.exports = (next) => {
+  RESTobj.Query({
+    Operation: "ItemLookup",
+    ItemId: 'B074SGVDK3',
+    ResponseGroup: "Small"
+  }, function(err, results) {
+    if(err){
+      console.log(err);
+    }else{
+      console.log('results Item: ', results[0]['Item']);
+      next(results[0]['Item']);
+    }
+  });
+};
