602f33edce434304af47aff559f409c695973aea
edbertlinardi <elinardi7@hotmail.com>
2017-11-30 20:27:31 -0600

    api call

---
+++ backend/aws-api/api-call.js
@@ -0,0 +1,16 @@
+amazon = require('aws-product-api');
+
+var RESTobj = new amazon("UxE++3FH2C8wTyYyOwOKloP7bQZOumol329NNiHr", "AKIAIL6MEBROY6QG653Q", " cs498rkfp-20");
+
+RESTobj.Query({
+  Operation: "ItemLookup",
+  ItemId: 'B074SGVDK3',
+  ResponseGroup: "Small"
+}, function(err, results) {
+  if(err){
+    console.log(err);
+  }else{
+    console.log('abc');
+    console.log(results[0]['Item']);
+  }
+});
