eafacf188df1ad0b95728ab3295cb4a1146dac2b
Bo Zheng <bozheng@Bos-MacBook-Pro.local>
2017-12-09 00:32:50 -0600

    get email

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -116,3 +116,124 @@ class Dashboard extends Component {
 }
 
 export default Dashboard
+
+/**
+'use strict';
+
+import React, { Component } from 'react'
+import { Button, Card } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+import styles from './styles.scss'
+import Carousel from 'nuka-carousel'
+
+ 
+import { GetItem, firstItem, likeItem, dislikeItem, wishListItem } from '../../../../backend/main.js'
+//get email in the props and call axios get to get the wishlist //
+
+class Dashboard extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: true
+        }
+        this.logOut = this.logOut.bind(this);
+        this.like = this.like.bind(this);
+        this.dislike = this.dislike.bind(this);
+        this.addToWishlist = this.addToWishlist.bind(this);
+        this.handleProp=this.handleProp.bind(this);
+    }
+
+    handleProp(props) {
+          console.log('get email from props' + this.props.location.state.email);
+          //return props.Empnumber;
+    }
+
+    componentWillMount() {
+        console.log('this is the first item');
+        let itemGetter = new GetItem(0);
+        firstItem(itemGetter, (result) => {
+            console.log('first = ',JSON.stringify(result));
+        });
+    }
+
+    componentDidMount() {
+        
+
+        axios.get('/api/profile').then( (res) => {
+            console.log(res);
+            this.setState({
+                isLoggedIn: true
+            })
+        }).catch( (err) => {
+            this.setState({
+                isLoggedIn: false
+            })
+        })
+    }
+
+    logOut() {
+        axios.get('/api/logout').then( (res) => {
+            console.log("Logged out");
+        })
+    }
+    like(){
+      console.log('like button clicked');
+    }
+
+    dislike(){
+      console.log('dislike button clicked');
+    }
+    addToWishlist(){
+      console.log('addToWishlist button clicked');
+    }
+
+    render() {
+
+        if (this.state.isLoggedIn) {
+            return(
+                <div className="Dashboard">
+                    <Card>
+                        <h1>Welcome to the App!</h1>
+                        <p>You are now logged in.</p>
+                        <h1>This is the product page</h1>
+                        <Link to="/wishlist">
+                            Wishlist
+                        </Link>
+                        <Link to="/" onClick={this.logOut}>
+                            Log out
+                        </Link>
+                        <Carousel>
+                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
+                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
+                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
+                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
+                        </Carousel>
+                    </Card>
+                        <button onClick={this.like}> like </button>
+                        <button onClick={this.dislike}> dislike </button>
+                        <button onClick={this.addToWishlist}> Add to wishlist </button>
+                        <button onClick={this.handleProp}> get email </button>
+                </div>
+
+            )
+        } else {
+            return(
+                <div className="Dashboard">
+                    <Card>
+                        <h1>The email or password is incorrect</h1>
+                        <Link to="/">
+                            Back
+                        </Link>
+                    </Card>
+                </div>
+            )
+        }
+    }
+}
+
+export default Dashboard
+
+**/
