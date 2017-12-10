50a0d7b8ab0c603c6ac46b78fb467ff919e8be54
edbertlinardi <elinardi7@hotmail.com>
2017-12-10 14:10:04 -0600

    check

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -17,14 +17,24 @@ class Dashboard extends Component {
         this.state = {
             isLoggedIn: true,
             itemGetter: {},
+            URL1:"",
+            URL2:"",
+            URL3:"",
+            URL4:"",
+            productName:""
         }
-
+        //this.showURL = this.showURL.bind(this);
         this.logOut = this.logOut.bind(this);
         this.like = this.like.bind(this);
         this.dislike = this.dislike.bind(this);
         this.addToWishlist = this.addToWishlist.bind(this);
-        this.storeToWishlist = this.storeToWishlist.bind(this);
         this.getFirstItem = this.getFirstItem.bind(this);
+        this.showimage = this.showimage.bind(this);
+    }
+
+
+    showimage(){
+        console.log(this.state.image);
     }
 
     getFirstItem() {
@@ -34,70 +44,117 @@ class Dashboard extends Component {
     }
 
     componentWillMount() {
-      // get the last category index from db
-      let newItemGetter = new GetItem(0);
-      this.setState({
-        itemGetter: newItemGetter,
-      })
-      firstItem(newItemGetter, (result) => {
-        console.log('first = ',result);
-      });
-    }
+       // get the last category index from db
+        let newItemGetter = new GetItem(0);
+        this.setState({
+          itemGetter: newItemGetter,
+        })
+        firstItem(newItemGetter, (result) => {
+          console.log('first = ',result);
+          let img = result[0].ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
+          let img2 = result[0].ImageSets[0].ImageSet;
+          let url1 = img2[0].LargeImage[0].URL[0];
+          let url2 = img2[1].LargeImage[0].URL[0];
+          let url3 = img2[2].LargeImage[0].URL[0];
+          let url4 = img2[3].LargeImage[0].URL[0];
+          let name = result[0].ItemAttributes[0].Title[0].toString();
+        this.setState({
+
+            URL1:url1,
+            URL2:url2,
+            URL3:url3,
+            URL4:url4,
+            productName: name
+        });
+        console.log('this.state.image',this.state.image);
+        //console.log('medium image',JSON.stringify(result[0].ImageSets[0].ImageSet[0].MediumImage[1]));
+        //console.log('image array', JSON.stringify(resultarray));
+        console.log('product ID ',JSON.stringify(result[0].ASIN));
+        });
+
+      }
 
     componentDidMount() {
-        axios.get('/api/profile').then( (res) => {
-            console.log(res);
-            this.setState({
-                isLoggedIn: true
-            })
-        }).catch( (err) => {
-            this.setState({
-                isLoggedIn: false
-            })
-        })
+
+
+      axios.get('/api/profile').then( (res) => {
+             console.log(res);
+             this.setState({
+                 isLoggedIn: true
+             })
+         }).catch( (err) => {
+             this.setState({
+                  isLoggedIn: false
+              })
+          })
     }
+
 
     logOut() {
         axios.get('/api/logout').then( (res) => {
             console.log("Logged out");
         })
     }
+
     like(){
-      // store last category index
-      storeLastCategoryIndex(this.props.location.state.email, this.state.itemGetter.categoryIndex);
-      console.log(this.state.itemGetter);
-      likeItem(this.state.itemGetter, (result) => {
-        console.log(result);
-      })
+         // store last category index
+       storeLastCategoryIndex(this.props.location.state.email, this.state.itemGetter.categoryIndex);
+       console.log(this.state.itemGetter);
+       likeItem(this.state.itemGetter, (result) => {
+         //console.log(result);
+         console.log('like = ',result);
+          let img = result[0].ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
+          let img2 = result[0].ImageSets[0].ImageSet;
+          let url1 = img2[0].LargeImage[0].URL[0];
+          let url2 = img2[1].LargeImage[0].URL[0];
+          let url3 = img2[2].LargeImage[0].URL[0];
+          let url4 = img2[3].LargeImage[0].URL[0];
+          let name = result[0].ItemAttributes[0].Title[0].toString();
+
+        this.setState({
+
+            URL1:url1,
+            URL2:url2,
+            URL3:url3,
+            URL4:url4,
+            productName: name
+        });
+        //window.load();
+       })
       console.log('like button clicked');
     }
 
     dislike(){
-      storeLastCategoryIndex(this.props.location.state.email, this.state.itemGetter.categoryIndex);
-      dislikeItem(this.state.itemGetter, (result) => {
-        console.log(result);
-      })
-        // store last category index
+        storeLastCategoryIndex(this.props.location.state.email, this.state.itemGetter.categoryIndex);
+        dislikeItem(this.state.itemGetter, (result) => {
+         //console.log(result);
+         console.log('dislike = ',result);
+          //let img = result[0].ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
+          let img2 = result[0].ImageSets[0].ImageSet;
+          let url1 = img2[0].LargeImage[0].URL[0];
+          let url2 = img2[1].LargeImage[0].URL[0];
+          let url3 = img2[2].LargeImage[0].URL[0];
+          let url4 = img2[3].LargeImage[0].URL[0];
+          let name = result[0].ItemAttributes[0].Title[0].toString();
+
+        this.setState({
+            URL1:url1,
+            URL2:url2,
+            URL3:url3,
+            URL4:url4,
+            productName: name
+        });
+       })
+         // store last category index
       console.log('dislike button clicked');
     }
 
-    storeToWishlist(email, id) {
-      console.log('store to wishlist');
-      var url = "http://localhost:3000/api/DB/" + String(email) + '/' + String(id);
-      axios
-                .post(url)
-                .then(response => {
-
-                });
-
-    }
-
     addToWishlist(){
-      storeLastCategoryIndex(this.props.location.state.email, this.state.itemGetter.categoryIndex);
-      this.storeToWishlist(this.props.location.state.email, this.state.itemGetter.curr._itemId);
-      wishListItem(this.state.itemGetter, (result) => {
-        console.log(result);
-      });
+       storeLastCategoryIndex(this.props.location.state.email, this.state.itemGetter.categoryIndex);
+       this.storeToWishlist(this.props.location.state.email, this.state.itemGetter.curr._itemId);
+       wishListItem(this.state.itemGetter, (result) => {
+         console.log(result);
+       });
       console.log('addToWishlist button clicked');
     }
 
@@ -105,30 +162,35 @@ class Dashboard extends Component {
 
         if (this.state.isLoggedIn) {
             //this.getFirstItem();
+            const imageUrl = this.state.image;
+            console.log('url  = ',imageUrl);
+            //const ele = <img width='200px' height='200px' src={this.state.image}/>;
+            //const bb = url(this.state.image);
             return(
                 <div className="Dashboard">
                     <Card>
-                        <h1>Welcome to the App!</h1>
-                        <p>You are now logged in.</p>
                         <h1>This is the product page</h1>
                         <Link to="/wishlist">
                             Wishlist
                         </Link>
+
                         <Link to="/" onClick={this.logOut}>
                             Log out
                         </Link>
+                        <h1>{this.state.productName}</h1>
                         <Carousel>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
+                              <img width='200px' height='200px' src={this.state.URL1}/>
+                              <img width='200px' height='200px' src={this.state.URL2}/>
+                              <img width='200px' height='200px' src={this.state.URL3}/>
+                              <img width='200px' height='200px' src={this.state.URL4}/>
                         </Carousel>
+
                     </Card>
                         <button onClick={this.like}> like </button>
                         <button onClick={this.dislike}> dislike </button>
                         <button onClick={this.addToWishlist}> Add to wishlist </button>
+                        <button onClick={this.showimage}> show image </button>
                 </div>
-
             )
         } else {
             return(
@@ -149,19 +211,15 @@ export default Dashboard
 
 /**
 'use strict';
-
 import React, { Component } from 'react'
 import { Button, Card } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-
 import styles from './styles.scss'
 import Carousel from 'nuka-carousel'
 
-
 import { GetItem, firstItem, likeItem, dislikeItem, wishListItem } from '../../../../backend/main.js'
 //get email in the props and call axios get to get the wishlist //
-
 class Dashboard extends Component {
 
     constructor(props) {
@@ -175,12 +233,10 @@ class Dashboard extends Component {
         this.addToWishlist = this.addToWishlist.bind(this);
         this.handleProp=this.handleProp.bind(this);
     }
-
     handleProp(props) {
           console.log('get email from props' + this.props.location.state.email);
           //return props.Empnumber;
     }
-
     componentWillMount() {
         console.log('this is the first item');
         let itemGetter = new GetItem(0);
@@ -188,9 +244,7 @@ class Dashboard extends Component {
             console.log('first = ',JSON.stringify(result));
         });
     }
-
     componentDidMount() {
-
 
         axios.get('/api/profile').then( (res) => {
             console.log(res);
@@ -203,7 +257,6 @@ class Dashboard extends Component {
             })
         })
     }
-
     logOut() {
         axios.get('/api/logout').then( (res) => {
             console.log("Logged out");
@@ -212,16 +265,13 @@ class Dashboard extends Component {
     like(){
       console.log('like button clicked');
     }
-
     dislike(){
       console.log('dislike button clicked');
     }
     addToWishlist(){
       console.log('addToWishlist button clicked');
     }
-
     render() {
-
         if (this.state.isLoggedIn) {
             return(
                 <div className="Dashboard">
@@ -247,7 +297,6 @@ class Dashboard extends Component {
                         <button onClick={this.addToWishlist}> Add to wishlist </button>
                         <button onClick={this.handleProp}> get email </button>
                 </div>
-
             )
         } else {
             return(
@@ -263,7 +312,5 @@ class Dashboard extends Component {
         }
     }
 }
-
 export default Dashboard
-
 **/
