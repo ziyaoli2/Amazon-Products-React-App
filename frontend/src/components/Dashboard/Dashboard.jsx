cf48df362ac8c1522488002b4200384b39884d32
Bo Zheng <bozheng@Bos-MacBook-Pro.local>
2017-12-10 21:17:17 -0600

    fix

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -116,12 +116,20 @@ class Dashboard extends Component {
           }
           if(typeof img2[1] !== 'undefined') {
             url2 = img2[1].LargeImage[0].URL[0];
+          }else{
+            url2 = url1;
           }
           if(typeof img2[2] !== 'undefined') {
             url3 = img2[2].LargeImage[0].URL[0];
           }
+          else{
+            url3 = url2;
+          }
           if(typeof img2[3] !== 'undefined') {
             url4 = img2[3].LargeImage[0].URL[0];
+          }
+          else{
+            url4 = url3;
           }
           if(typeof result[0].ItemAttributes[0].Title[0] !== 'undefined') {
             name = result[0].ItemAttributes[0].Title[0].toString();
@@ -173,13 +181,22 @@ class Dashboard extends Component {
           }
           if(typeof img2[1] !== 'undefined') {
             url2 = img2[1].LargeImage[0].URL[0];
+          }else{
+            url2 = url1;
           }
           if(typeof img2[2] !== 'undefined') {
             url3 = img2[2].LargeImage[0].URL[0];
           }
+          else{
+            url3 = url2;
+          }
           if(typeof img2[3] !== 'undefined') {
             url4 = img2[3].LargeImage[0].URL[0];
           }
+          else{
+            url4 = url3;
+          }
+          
           if(typeof result[0].ItemAttributes[0].Title[0] !== 'undefined') {
             name = result[0].ItemAttributes[0].Title[0].toString();
           }
@@ -265,117 +282,3 @@ class Dashboard extends Component {
 }
 
 export default Dashboard
-
-/**
-'use strict';
-import React, { Component } from 'react'
-import { Button, Card } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import styles from './styles.scss'
-import Carousel from 'nuka-carousel'
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 591176ea7968e428824358366dd5344e7b0c351a
-import { GetItem, firstItem, likeItem, dislikeItem, wishListItem } from '../../../../backend/main.js'
-//get email in the props and call axios get to get the wishlist //
-class Dashboard extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: true
-        }
-        this.logOut = this.logOut.bind(this);
-        this.like = this.like.bind(this);
-        this.dislike = this.dislike.bind(this);
-        this.addToWishlist = this.addToWishlist.bind(this);
-        this.handleProp=this.handleProp.bind(this);
-    }
-    handleProp(props) {
-          console.log('get email from props' + this.props.location.state.email);
-          //return props.Empnumber;
-    }
-    componentWillMount() {
-        console.log('this is the first item');
-        let itemGetter = new GetItem(0);
-        firstItem(itemGetter, (result) => {
-            console.log('first = ',JSON.stringify(result));
-        });
-    }
-    componentDidMount() {
-<<<<<<< HEAD
-=======
-
->>>>>>> 591176ea7968e428824358366dd5344e7b0c351a
-
-        axios.get('/api/profile').then( (res) => {
-            console.log(res);
-            this.setState({
-                isLoggedIn: true
-            })
-        }).catch( (err) => {
-            this.setState({
-                isLoggedIn: false
-            })
-        })
-    }
-    logOut() {
-        axios.get('/api/logout').then( (res) => {
-            console.log("Logged out");
-        })
-    }
-    like(){
-      console.log('like button clicked');
-    }
-    dislike(){
-      console.log('dislike button clicked');
-    }
-    addToWishlist(){
-      console.log('addToWishlist button clicked');
-    }
-    render() {
-        if (this.state.isLoggedIn) {
-            return(
-                <div className="Dashboard">
-                    <Card>
-                        <h1>Welcome to the App!</h1>
-                        <p>You are now logged in.</p>
-                        <h1>This is the product page</h1>
-                        <Link to="/wishlist">
-                            Wishlist
-                        </Link>
-                        <Link to="/" onClick={this.logOut}>
-                            Log out
-                        </Link>
-                        <Carousel>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"/>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
-                              <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
-                        </Carousel>
-                    </Card>
-                        <button onClick={this.like}> like </button>
-                        <button onClick={this.dislike}> dislike </button>
-                        <button onClick={this.addToWishlist}> Add to wishlist </button>
-                        <button onClick={this.handleProp}> get email </button>
-                </div>
-            )
-        } else {
-            return(
-                <div className="Dashboard">
-                    <Card>
-                        <h1>The email or password is incorrect</h1>
-                        <Link to="/">
-                            Back
-                        </Link>
-                    </Card>
-                </div>
-            )
-        }
-    }
-}
-export default Dashboard
-**/
